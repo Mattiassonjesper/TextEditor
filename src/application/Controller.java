02f70c3bcf53165ef5253e73ca2dda162609721c
Mattiassonjesper <57625574+Mattiassonjesper@users.noreply.github.com>
2020-10-27 12:20:14 +0100

    Added functionality to slider

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,15 +1,23 @@
 package application;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
+import javafx.scene.text.Font;
 
 public class Controller {
 
 	@FXML
 	private TextArea areaText;
 
+	@FXML
 	private Model editorModel;
+
+	@FXML
+	private Slider S;
 
 	public Controller(Model editorModel) {
 		this.editorModel = editorModel;
@@ -38,6 +46,21 @@ public class Controller {
 		alert.setTitle("About");
 		alert.setContentText("This is a text editor");
 		alert.show();
+	}
+
+	@FXML
+	public void onSlide() {
+		System.out.println("Slider be Sliding!");
+		S.setMax(100);
+		S.setMin(1);
+		S.setValue(20);
+
+		S.valueProperty().addListener(new ChangeListener<Number>() {
+			public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
+				areaText.setFont(Font.font("SansSerif", S.getValue()));
+
+			}
+		});
 	}
 
 }
